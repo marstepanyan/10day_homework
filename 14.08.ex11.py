c88620f6a0ba0fe709528ab2f78cc4ffda44c883
marstepanyan <102877297+marstepanyan@users.noreply.github.com>
2022-08-14 21:54:58 +0400

    Add files via upload

---
+++ 14.08.ex11.py
@@ -0,0 +1,9 @@
+def square_of_sum(n):
+    return ((n * (n + 1)) // 2) ** 2
+
+
+def sum_of_squares(n):
+    return (n * (n + 1) * (2 * n + 1)) // 6
+
+
+print(square_of_sum(100) - sum_of_squares(100))
