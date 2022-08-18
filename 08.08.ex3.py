7d38bce04d6c813ba6dc0455a26fb45a7280d468
marstepanyan <102877297+marstepanyan@users.noreply.github.com>
2022-08-18 13:35:52 +0000

    Update 08.08.ex3.py

--- 08.08.ex3.py
+++ 08.08.ex3.py
@@ -2,10 +2,17 @@
 def shortest_subarray(arr):
     degree = max(arr.count(item) for item in arr)
     indexes = [i for i, item in enumerate(arr) if arr.count(item) == degree]
-    right_index = indexes[-1]
-    left_index = indexes[0]
-    # print(arr[left_index:right_index + 1])
-    return len(arr[left_index:right_index + 1])
+
+    unique_idx = []
+    for item in arr:
+        if arr.index(item) in indexes and arr.index(item) not in unique_idx:
+            unique_idx.append(arr.index(item))
+
+    left_idx = max(unique_idx)
+    key = arr[left_idx]
+    right_idx = len(arr) - 1 - arr[::-1].index(key)
+    # print(arr[left_idx:right_idx + 1])
+    return len(arr[left_idx:right_idx + 1])
 
 
 nums = [1, 2, 2, 1, 0, 2, 3]
